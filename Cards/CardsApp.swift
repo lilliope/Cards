dbb68ee7d3358264b23954aeff8af786fedd6f7f
Lily Ton <loi.nguyen1099@gmail.com>
2026-02-11 18:42:19 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Loi Ton on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
